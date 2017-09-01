22de4821bada3130ba1b30a084ac714784b3198f
Xinlin Li <1161537463@qq.com>
2017-09-01 12:29:30 +1000

    Put old code into "old" for HW4.

--- HW4/homework4.py
+++ HW4/homework4.py
@@ -8,19 +8,10 @@
 ## returns the correct answer to the homework problem. (The statement
 ## "return 1" is just a placeholder: you should of course modify it.)
 def max_relative_frequency(s):
-    # import string # No need of this in new method.
     # In case there are some upper letters.
     s = s.lower()
     length_of_letter = 0
     highest_relative_frequency = 0
-    '''
-    for i in range(len(s)):
-        # Count Only letters of the English alphabet,no other languages
-        # import string needed for the function ASCII
-        if s[i] in string.ascii_letters:
-            length_of_letter = length_of_letter + 1
-     # No need of this in new method.
-    '''
     for i in range(97, 123):
         # Count letter appeared times ONLY from a to z and add to length.
         length_of_letter = length_of_letter + s.count(chr(i))
